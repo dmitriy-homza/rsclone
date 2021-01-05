0f425d8cedd75fce1f54bbe39924f1a607af0856
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2021-01-05 22:57:33 +0300

    add: scss

--- Terassa/src/pages/index.js
+++ Terassa/src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import "./styles/index.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <h2>Hello world!</h2>
+    </div>
+  )
 }
