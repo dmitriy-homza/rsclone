9b4616089eb4160634a53d4786cbbea1918a8696
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2021-01-13 11:13:35 +0300

    add: main page

--- Terassa/src/components/footer.jsx
+++ Terassa/src/components/footer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
-export default () => (
-  <footer>
-    <Link to="/">Main</Link>
-    That is footer
-  </footer>
-);
+export default function Footer() {
+  return (
+    <footer>
+      <div className="wrapper">
+        <Navbar className="w-100 justify-content-between" light expand="md">
+          <NavbarBrand className="garamond">
+            <Link to="/">Terassa</Link>
+          </NavbarBrand>
+        </Navbar>
+      </div>
+    </footer>
+  );
+}
--- Terassa/src/pages/index.jsx
+++ Terassa/src/pages/index.jsx
@@ -3,6 +3,8 @@ import { Helmet } from 'react-helmet';
 import { Button } from 'reactstrap';
 import Layout from '../components/layout';
 import '../styles/index.scss';
+import imgSoup from '../images/soup.png';
+import imgEnvironment from '../images/environment.png';
 
 export default () => (
   <>
@@ -20,13 +22,37 @@ export default () => (
           </div>
         </section>
         <section className="delicious-food">
-          <div className="content-block">
-            <Button color="primary">To order</Button>
+          <div className="content-block w-100">
+            <div className="col-3 col-md-5">
+              <img src={imgSoup} alt="Soup" width="450px" />
+            </div>
+            <div className="col-9 col-md-7 pl-5">
+              <h2 className="garamond">Delicious food</h2>
+              <p>
+                Galaxies astonishment tendrils of gossamer clouds dream of the minds eye a very
+                small stage in a vast cosmic arena light years. Network of wormholes courage of our
+                questions courage of our questions paroxysm of global death venture bits of moving
+                fluff.
+              </p>
+              <Button color="primary">To order</Button>
+            </div>
           </div>
         </section>
         <section className="pleasant-env">
-          <div className="content-block">
-            <Button color="primary">To order</Button>
+          <div className="content-block w-100">
+            <div className="col-12 col-md-7 pl-5">
+              <h2 className="garamond">Pleasant environment</h2>
+              <p>
+                Galaxies astonishment tendrils of gossamer clouds dream of the minds eye a very
+                small stage in a vast cosmic arena light years. Network of wormholes courage of our
+                questions courage of our questions paroxysm of global death venture bits of moving
+                fluff.
+              </p>
+              <Button color="primary">To order</Button>
+            </div>
+            <div className="col-2 col-md-5">
+              <img src={imgEnvironment} alt="Soup" width="450px" />
+            </div>
           </div>
         </section>
       </main>
