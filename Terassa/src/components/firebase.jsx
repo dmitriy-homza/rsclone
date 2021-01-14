ee8c124f733a336059b648566b17b60c881f2bf1
Qairkhan <shyngys94@gmail.com>
2021-01-14 20:09:52 +0300

    feat: add firebase token

---
+++ Terassa/src/components/firebase.jsx
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDp14hO-HbwV8OMry7oiR86NFQ8-Hehei0',
+  authDomain: 'terassa-project.firebaseapp.com',
+  databaseURL: 'https://terassa-project-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'terassa-project',
+  storageBucket: 'terassa-project.appspot.com',
+  messagingSenderId: '82471937161',
+  appId: '1:82471937161:web:72b67c83795cd5d6a3d258',
+};
+firebase.initializeapp(firebaseConfig);
--- Terassa/src/pages/login.jsx
+++ Terassa/src/pages/login.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+import { Helmet } from 'react-helmet';
+import {
+  InputGroup, InputGroupAddon, InputGroupText, Input,
+} from 'reactstrap';
 import Layout from '../components/layout';
 import '../styles/index.scss';
 
-export default () => (
-  <>
-    <Layout>
-      <h1>Login</h1>
-    </Layout>
-  </>
-);
+
+export default class login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  componentDidMount() {
+    const db = firebase.database();
+  }
+  handleChange = ({target: {value, id}}) => {
+    this.setState ({[id]: value,
+    })
+  };
+
+  
+  render() {
+    return (
+      <div>
+        {' '}
+        <Helmet title="Terassa - Login" />
+        <Layout>
+          <div>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText>@</InputGroupText>
+              </InputGroupAddon>
+              <Input placeholder="email" id="email" type="text" onChange={this.handleChange}/>
+              <Input placeholder="password" id="password" type="password" onChange={this.handleChange}/>
+              <Input type="submit" />
+            </InputGroup>
+          </div>
+        </Layout>
+      </div>
+    );
+  }
+}
