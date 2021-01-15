c2a31550340d003e1d0766eb0d7cf10d0224aa81
RomanFilipenia <biologg@mail.ru>
2021-01-15 20:32:04 +0300

    add: order

---
+++ Terassa/src/pages/order.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import '../styles/order.scss';
+import Order from '../components/order/order';
+
+export default () => (
+  <>
+    <Layout>
+      <Order />
+      <h1>Order</h1>
+    </Layout>
+  </>
+);
