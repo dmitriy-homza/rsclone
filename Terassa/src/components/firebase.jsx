eb7543507bdc769cb152547f744d6cfc0e53effe
Qairkhan <shyngys94@gmail.com>
2021-01-16 00:22:35 +0300

    feat: firebase base added

--- Terassa/src/components/firebase.jsx
+++ Terassa/src/components/firebase.jsx
@@ -1,12 +1,18 @@
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDp14hO-HbwV8OMry7oiR86NFQ8-Hehei0',
-  authDomain: 'terassa-project.firebaseapp.com',
-  databaseURL: 'https://terassa-project-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'terassa-project',
-  storageBucket: 'terassa-project.appspot.com',
-  messagingSenderId: '82471937161',
-  appId: '1:82471937161:web:72b67c83795cd5d6a3d258',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
-firebase.initializeapp(firebaseConfig);
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- Terassa/src/pages/login.jsx
+++ Terassa/src/pages/login.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import * as firebase from 'firebase';
 
 import { Helmet } from 'react-helmet';
 import {
   InputGroup, InputGroupAddon, InputGroupText, Input,
 } from 'reactstrap';
+import firebase from '../components/firebase';
 import Layout from '../components/layout';
 import '../styles/index.scss';
-
 
 export default class login extends Component {
   constructor(props) {
@@ -20,13 +19,21 @@ export default class login extends Component {
 
   componentDidMount() {
     const db = firebase.database();
+    console.log('db', db);
   }
-  handleChange = ({target: {value, id}}) => {
-    this.setState ({[id]: value,
-    })
+
+  handleChange = ({ target: { value, id } }) => {
+    this.setState({ [id]: value });
   };
 
-  
+  createAccount = () => {
+    const { email, password } = this.state;
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => console.log(error));
+  };
+
   render() {
     return (
       <div>
@@ -38,9 +45,14 @@ export default class login extends Component {
               <InputGroupAddon addonType="prepend">
                 <InputGroupText>@</InputGroupText>
               </InputGroupAddon>
-              <Input placeholder="email" id="email" type="text" onChange={this.handleChange}/>
-              <Input placeholder="password" id="password" type="password" onChange={this.handleChange}/>
-              <Input type="submit" />
+              <Input placeholder="email" id="email" type="text" onChange={this.handleChange} />
+              <Input
+                placeholder="password"
+                id="password"
+                type="password"
+                onChange={this.handleChange}
+              />
+              <Input type="submit" onClick={this.createAccount} />
             </InputGroup>
           </div>
         </Layout>
