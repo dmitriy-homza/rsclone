a4a5b31c93f1bd795dc2793e3c26c3e8ae9a4727
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2021-01-17 20:41:17 +0300

    add: propses

--- Terassa/src/components/additional/card.jsx
+++ Terassa/src/components/additional/card.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button,
 } from 'reactstrap';
 
-export default () => (
+const card = ({
+  name, weight, img, description, cost,
+}) => (
   <>
     <Card>
-      <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+      <CardImg top width="100%" src={img} alt="Card image cap" />
       <CardBody>
-        <CardTitle tag="h5">Card title</CardTitle>
-        <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-        <CardText>make up the bulk of the cards content.</CardText>
+        <CardTitle tag="h5">{name}</CardTitle>
+        <CardSubtitle tag="h6" className="mb-2 text-muted">{weight}</CardSubtitle>
+        <CardText>{description}</CardText>
+        <div className="cost">{cost}</div>
         <input
           type="time"
           id="appt"
@@ -27,3 +31,13 @@ export default () => (
     </Card>
   </>
 );
+
+card.propTypes = {
+  name: PropTypes.string,
+  weight: PropTypes.string,
+  img: PropTypes.string,
+  description: PropTypes.string,
+  cost: PropTypes.string,
+};
+
+export default card;
--- Terassa/src/components/additional/options.jsx
+++ Terassa/src/components/additional/options.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Card from './card';
+import additionals from './additional.json';
 
-export default () => (
+const options = () => (
   <>
     <div className="dishes">
       <h2>Dishes</h2>
       <div className="dishes-wrapper">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {
+          Object.keys(additionals).map((key) => (additionals[key].map((item) => (
+            <Card
+              name={item.name}
+            />
+          ))
+          ))
+ }
       </div>
     </div>
   </>
 );
+
+export default options;
