1641d0b18cb8876a8d586714a21c6bd5737e0fff
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2021-02-02 01:25:44 +0300

    add: book mark up

--- Terassa/src/pages/book.jsx
+++ Terassa/src/pages/book.jsx
@@ -4,6 +4,9 @@
 /* eslint-disable max-len */
 /* eslint-disable no-param-reassign */
 import React, { useState } from 'react';
+import {
+  Alert, Form, Col, Row, FormGroup, Label, Input,
+} from 'reactstrap';
 import ReactNotification, { store } from 'react-notifications-component';
 import Layout from '../components/layout';
 import Options from '../components/additional/options';
@@ -48,36 +51,76 @@ export default () => {
     <>
       <Layout>
         <ReactNotification />
-        <main className="d-flex" />
-        <input type="date" id="date" min="2018-01-01" max="2018-12-31" />
-        <input type="time" id="time" name="appt" min="09:00" max="18:00" required />
-        <button
-          onClick={() => {
-            if (document.getElementById('date').value && document.getElementById('time').value) {
-              setPage({
-                tables: 'Столы',
-                visitTime: `${document.getElementById('date').value}T${
-                  document.getElementById('time').value
-                }+03:00`,
-              });
-            } else {
-              store.addNotification({
-                title: 'Choose time!',
-                message: 'Choose a time to visit and book table',
-                type: 'warning',
-                insert: 'top',
-                container: 'top-right',
-                animationIn: ['animate__animated animate__fadeIn'],
-                animationOut: ['animate__animated animate__fadeOut'],
-                dismiss: {
-                  duration: 3000,
-                },
-              });
-            }
-          }}
-        >
-          To additions
-        </button>
+        <main className="first-book p-5 flex-column d-flex">
+          <h2>Book:</h2>
+          <section className="date-choose">
+            <Alert color="primary">
+              Please choose a time for your visit:
+            </Alert>
+            <Form>
+              <Row form>
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="date">Date</Label>
+                    <Input
+                      type="date"
+                      name="date"
+                      id="date"
+                      placeholder="date placeholder"
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md={4}>
+                  <FormGroup>
+                    <Label for="time">Time</Label>
+                    <Input
+                      type="time"
+                      step="600"
+                      name="time"
+                      min="08:00"
+                      max="22:00"
+                      id="time"
+                      placeholder="time placeholder"
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+            </Form>
+          </section>
+          <section>
+            <Alert color="primary">
+              And choose a table:
+            </Alert>
+          </section>
+
+          <button
+            onClick={() => {
+              if (document.getElementById('date').value && document.getElementById('time').value) {
+                setPage({
+                  tables: 'Столы',
+                  visitTime: `${document.getElementById('date').value}T${
+                    document.getElementById('time').value
+                  }+03:00`,
+                });
+              } else {
+                store.addNotification({
+                  title: 'Choose time!',
+                  message: 'Choose a time to visit and book table',
+                  type: 'warning',
+                  insert: 'top',
+                  container: 'top-right',
+                  animationIn: ['animate__animated animate__fadeIn'],
+                  animationOut: ['animate__animated animate__fadeOut'],
+                  dismiss: {
+                    duration: 3000,
+                  },
+                });
+              }
+            }}
+          >
+            Continue
+          </button>
+        </main>
       </Layout>
     </>
   ) : (
