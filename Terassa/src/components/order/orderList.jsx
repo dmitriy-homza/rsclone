04650ef1c8fcfc74c924d4f1a9129726a1a5a85e
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2021-02-02 10:18:10 +0300

    add: orders

--- Terassa/src/components/order/orderList.jsx
+++ Terassa/src/components/order/orderList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable array-callback-return */
 /* eslint-disable no-unused-vars */
 import React from 'react';
@@ -6,6 +7,7 @@ import {
   Table,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
+import BasketElement from '../additional/basketElement';
 import MenuDishes from './menuDishes';
 // import OrderItem from './orderItem';
 import Options from './options';
@@ -15,17 +17,23 @@ function OrderList({ order }) {
   const {
     additions, tables, visit,
   } = order;
+  const time = new Date(visit).toString();
   return (
     <>
       <Alert color="success">
-        Ваш заказ на
+        Your order on
         {' '}
-        {new Date(visit)}
+        <span>{time}</span>
         {' '}
-        найден!
+        founded!
       </Alert>
       <h5>{tables}</h5>
-      {/* {Object.keys(additions).map((group) => additions[group])} */}
+      {Object.keys(additions).map((group) => (
+        <>
+          <h3>{group}</h3>
+          {Array.from(additions[group]).map((element) => <BasketElement key={element.id} position={element} remover={() => {}} />)}
+        </>
+      ))}
       {/* <TabPane tabId="1">
         <Table>
           <thead>
